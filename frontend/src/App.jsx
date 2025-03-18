ceba82e8f57b54870ce9fa78a46a47ecb3e0aa44
anassazz <anass.azz2030@gmail.com>
2025-03-18 23:32:49 +0100

    add routes  paths

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import Navbar from "./components/Navbare/Navbar";
+import { Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import Projects from "./components/Projects/Projects";
+import Tasks from "./components/Tasks/Tasks";
+import Resources from "./components/Resources/Resources";
+import ProjectForm from "./components/Projects/ProjectForm";
+import TaskForm from "./components/Tasks/TaskForm";
+import ResourceForm from "./components/Resources/ResourceForm";
+
+function App() {
+  return (
+    <>
+      
+
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/project" element={<Projects></Projects>} />
+        <Route path="/tasks" element={<Tasks></Tasks>} />
+        <Route path="/resource" element={<Resources></Resources>} />
+
+        <Route path="/projectform" element={<ProjectForm></ProjectForm>} />
+        <Route path="/taskform" element={<TaskForm></TaskForm>} />
+        <Route path="/resourceform" element={<ResourceForm></ResourceForm>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
