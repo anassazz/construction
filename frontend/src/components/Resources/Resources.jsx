8edc0a64a6f08af34147120b5c4ac5d5e091efe7
anassazz <anass.azz2030@gmail.com>
2025-03-19 01:20:56 +0100

    fetchResources

--- frontend/src/components/Resources/Resources.jsx
+++ frontend/src/components/Resources/Resources.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Plus, Pencil, Trash2 } from "lucide-react";
 import {Link} from "react-router-dom";
 
 function Resources() {
+
+  const [resources, setResources] = useState([]);
+
+  useEffect(() => {
+    const fetchResources = async () => {
+      try {
+        const response = await axios.get("http://127.0.0.1:3000/api/resources");
+        setResources(response.data); 
+      } catch (error) {
+        console.error(
+          "Error fetching projects:",
+          error.response?.data || error.message
+        );
+      }
+    };
+
+    fetchResources(); 
+  }, []);
+
+
+
+
+
+
   return (
     <div>
       <div>
@@ -17,6 +42,54 @@ function Resources() {
         </Link>
       </div>
     </div>
+
+
+
+{/* Table to display the list of projects */}
+<div className="max-w-7xl mx-auto mt-10 p-6   shadow-lg ">
+        <h2 className="text-2xl font-semibold mb-9 text-center ">
+           Resource List
+        </h2>
+
+        <table className="min-w-full table-auto ">
+          <thead>
+            <tr >
+              <th className="px-4 py-2 border-b text-left">Resource Name </th>
+              <th className="px-4 py-2 border-b text-left">Select a type</th>
+              <th className="px-4 py-2 border-b text-left">Quantity</th>
+              <th className="px-4 py-2 border-b text-left">Supplier</th>
+            </tr>
+          </thead>
+          <tbody>
+            {resources.length > 0 ? (
+              resources.map((resource) => (
+                <tr key={resources._id}>
+                  <td className="px-4 py-2 border-b">{resource.resourceName}</td>
+                  <td className="px-4 py-2 border-b">{resource.type}</td>
+                  <td className="px-4 py-2 border-b">{resource.quantity}</td>
+                  <td className="px-4 py-2 border-b">{resource.supplier}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="5" className="px-4 py-2 border-b text-center">
+                  No projects found.
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+
+
+
+
+
+
+
+
+
+
     </div>
   )
 }
